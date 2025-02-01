4afb16960a9a02be01f79f32ac5934ee9c65775d
Aashish Prajapati <aashishprajapati50@gmail.com>
2025-02-01 19:46:46 +0530

    Delete Interface Programs/interfacMobilePhoneSIMCardInteraction directory

--- Interface Programs/interfacMobilePhoneSIMCardInteraction/SIMCard.java
+++
@@ -1,9 +0,0 @@
-package interfacMobilePhoneSIMCardInteraction;
-
-public interface SIMCard
-{
- long gettingPhoneNumber();
- void getnetworkProvider();
- void activation();
- void deactivation();
-}
